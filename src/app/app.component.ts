92482a0aa5b00c0b4f8040afeff85570c2e246e0
Jereez <ajereez@gmail.com>
2022-01-05 20:58:57 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,9 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
+import { Product } from './home.model';
 import { HomeService } from './home.service';
-export interface PeriodicElement {
-  name: string;
-  amount: number;
-  email: string;
-  mobile: number;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { amount: 1, name: 'Hydrogen', email: "ajereez@gmail.com", mobile: 9659071436 },
-  { amount: 1, name: 'Hydrogen', email: "ajereez@gmail.com", mobile: 9659071436 }
-];
+const ELEMENT_DATA: Product[] = [];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -23,16 +14,27 @@ export class AppComponent {
   title = 'eauction';
   emailFormControl = new FormControl('', [Validators.required]);
   displayedColumns: string[] = ['amount', 'name', 'email', 'mobile'];
-  arrayList:any[]=[];
-  dataSource = new MatTableDataSource<any>(this.arrayList)
+  arrayList:Product[]=[];
+  dataSource = new MatTableDataSource<Product>(this.arrayList)
   public constructor(private service: HomeService,private snackbar:MatSnackBar) {
 
   }
   onSubmit() {
     if(this.emailFormControl.valid){
-      this.service.getProduct(this.emailFormControl.value).subscribe((data: any) => {
+      let b = {
+        "firstName":"string",
+        "lastName":"string",
+        "address":"",
+        "city":"",
+        "state":"",
+        "phone":"dvfsv",
+        "email":"XVcdav",
+        "productID":"",
+        "bidAmount":"2000"
+      }
+      this.service.getProduct(this.emailFormControl.value).subscribe((data: Product[]|any) => {
         if(data.length>0){
-          this.arrayList = ELEMENT_DATA;
+          this.arrayList = data;
           this.dataSource = new MatTableDataSource<any>(this.arrayList)
         }else{
            this.snackbar.open("Product not found","X",{duration:3000});
---
+++ src/app/home.model.ts
@@ -0,0 +1,11 @@
+export class Product {
+    public firstName!: string;
+    public lastName!: string;
+    public address!: string;
+    public city!: string;
+    public state!: string;
+    public phone!: string;
+    public email!: string;
+    public productID!: string;
+    public bidAmount!: string;
+}
