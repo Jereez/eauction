99d1f3e7d5806b0369faf9c87f5152c132e6c278
Jereez <ajereez@gmail.com>
2022-01-05 17:36:17 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+export interface PeriodicElement {
+  name: string;
+  amount: number;
+  email: string;
+  mobile: number;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {amount: 1, name: 'Hydrogen', email: "ajereez@gmail.com", mobile: 9659071436},
+  {amount: 1, name: 'Hydrogen', email: "ajereez@gmail.com", mobile: 9659071436}
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eauction';
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  displayedColumns: string[] = ['amount', 'name', 'email', 'mobile'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatInputModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
